e88945137988fe14f571a5b1fd0687447f1c719a
Alexandre Junior <alejunior.dev@gmail.com>
2025-03-12 11:39:10 -0300

    feat: ensure user can not create transactiopns if above limit

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -6,13 +6,17 @@ import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ScrollArea } from "../_components/ui/scroll-area";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
+
   const transactions = await db.transaction.findMany({ where: { userId } });
+  const canAddTransaction = await canUserAddTransaction();
+
   return (
     <>
       <Navbar />
@@ -20,7 +24,7 @@ const TransactionsPage = async () => {
       <div className="space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between p-6">
           <h2 className="text-2xl font-bold">Transações</h2>
-          <AddTransactionButton />
+          <AddTransactionButton userCanAddTransaction={canAddTransaction} />
         </div>
         <ScrollArea>
           <DataTable columns={transactionColumns} data={transactions} />
