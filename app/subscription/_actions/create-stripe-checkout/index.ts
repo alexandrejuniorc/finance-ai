74c63949af0a23f63263720e3b09d9866a3c6440
Alexandre Junior <alejunior.dev@gmail.com>
2025-03-17 23:45:23 -0300

    fix: stripe api version to deploy

--- app/subscription/_actions/create-stripe-checkout/index.ts
+++ app/subscription/_actions/create-stripe-checkout/index.ts
@@ -15,7 +15,7 @@ export const createStripeCheckout = async () => {
   }
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2025-02-24.acacia",
+    apiVersion: "2024-10-28.acacia",
   });
 
   const session = await stripe.checkout.sessions.create({
